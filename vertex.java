a3fd143a1e50e4f862e694237f186e02269c3178
FawwasSabil25 <fawwas2573@gmail.com>
2024-05-16 19:32:14 +0700

    Mengerjakan Latihan Pertemuan 12

---
+++ vertex.java
@@ -0,0 +1,30 @@
+public class vertex {
+    private char label; //label (e.g. 'A')
+    private boolean wasVisited; 
+
+    public vertex(char lab){//konstruktor
+        label = lab;
+        wasVisited = false;
+    }
+
+    // Getter utk label
+    public char getLabel() {
+        return label;
+    }
+
+    // Setter utk label
+    public void setLabel(char label) {
+        this.label = label;
+    }
+
+    // Getter utk wasVisited
+    public boolean isWasVisited() {
+        return wasVisited;
+    }
+
+    // Setter utk wasVisited
+    public void setWasVisited(boolean wasVisited) {
+        this.wasVisited = wasVisited;
+    }
+
+}
